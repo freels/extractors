ea99d59fd10de67d110645f8e6954f124b8fe53f
Matt Freels <matt@freels.name>
2010-09-09 22:36:43 -0700

    add specs

--- project/build/ExtractorProject.scala
+++ project/build/ExtractorProject.scala
@@ -1,5 +1,10 @@
 import sbt._
+import com.twitter.sbt.StandardProject
 
-class ExtractorProject(info: ProjectInfo) extends StandardProject(info) with TemplateProject {
+class ExtractorProject(info: ProjectInfo) extends DefaultProject(info) with TemplateProject {
+  override def compileOptions = super.compileOptions ++ Seq(Unchecked)
 
+  val scalaToolsTesting  = "testing.scala-tools.org" at "http://scala-tools.org/repo-releases/testing/"
+
+  val specs = "org.scala-tools.testing" % "specs" % "1.6.2.1" % "test"
 }
