0ca16a11951575af0c7188598889cdefc77fea79
Matt Freels <matt@freels.name>
2011-02-17 12:59:03 -0800

    update project stuff

--- project/build/ExtractorProject.scala
+++ project/build/ExtractorProject.scala
@@ -1,13 +1,7 @@
 import sbt._
-import com.twitter.sbt.StandardProject
+import com.twitter.sbt.{StandardProject,TemplateProject}
 
-class ExtractorProject(info: ProjectInfo) extends DefaultProject(info) with TemplateProject {
-  override def compileOptions = super.compileOptions ++ Seq(Unchecked)
+class ExtractorProject(info: ProjectInfo) extends StandardProject(info) with TemplateProject {
 
-  val scalaTools  = "testing.scala-tools.org" at "http://scala-tools.org/repo-releases"
-
-  val specs = buildScalaVersion match {
-    case "2.8.0" => "org.scala-tools.testing" %% "specs" % "1.6.5" % "test"
-    case "2.7.7" => "org.scala-tools.testing" % "specs" % "1.6.2.1" % "test"
-  }
+  val specs = "org.scala-tools.testing" % "specs_2.8.1" % "1.6.6" % "test"
 }
