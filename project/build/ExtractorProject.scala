10958bd00340a86ac29b276462a7189694acafd4
Matt Freels <matt@freels.name>
2011-02-23 11:02:24 -0800

    for publishing

--- project/build/ExtractorProject.scala
+++ project/build/ExtractorProject.scala
@@ -1,7 +1,10 @@
 import sbt._
-import com.twitter.sbt.{StandardProject,TemplateProject}
+import com.twitter.sbt._
 
-class ExtractorProject(info: ProjectInfo) extends StandardProject(info) with TemplateProject {
+class ExtractorProject(info: ProjectInfo) extends StandardProject(info)
+with TemplateProject
+//with DefaultRepos
+with SubversionPublisher {
 
   val jackson    = "org.codehaus.jackson" % "jackson-core-asl"   % "1.7.3" % "provided"
   val jacksonMap = "org.codehaus.jackson" % "jackson-mapper-asl" % "1.7.3" % "provided"
@@ -11,4 +14,15 @@ class ExtractorProject(info: ProjectInfo) extends StandardProject(info) with Tem
   val objenesis = "org.objenesis"           % "objenesis"   % "1.1"   % "test"
   val specs     = "org.scala-tools.testing" % "specs_2.8.1" % "1.6.6" % "test"
   val jmock     = "org.jmock"               % "jmock"       % "2.4.0" % "test"
+
+  override def pomExtra =
+    <licenses>
+      <license>
+        <name>Apache 2</name>
+        <url>http://www.apache.org/licenses/LICENSE-2.0.txt</url>
+        <distribution>repo</distribution>
+      </license>
+    </licenses>
+
+  override def subversionRepository = Some("http://svn.local.twitter.com/maven-public")
 }
